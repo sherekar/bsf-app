ae0226ba7aacee7619060740047d82ab56e63f5d
sherekar <prasanna.sherekar@gmail.com>
2022-11-12 20:46:26 +0530

    added test cases and jacoco test coverage report

--- src/test/java/com/bsf/app/BsfApplicationTests.java
+++ src/test/java/com/bsf/app/BsfApplicationTests.java
@@ -1,13 +1,27 @@
 package com.bsf.app;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.bsf.app.controller.AccountController;
+import com.bsf.app.controller.TransactionController;
 
 @SpringBootTest
 class BsfApplicationTests {
 
+	@Autowired
+	private AccountController accountController;
+
+	@Autowired
+	private TransactionController transactionController;
+
 	@Test
 	void contextLoads() {
+		assertThat(accountController).isNotNull();
+		assertThat(transactionController).isNotNull();
 	}
 
 }
